8bc9d32edeee4f9e1496f53b840b0f6f436d46f5
Alexander <alexander.volkers@knowit.se>
2022-10-26 18:50:17 +0200

    query

---
+++ src/graphql/queries.ts
@@ -0,0 +1,33 @@
+import {gql} from '@apollo/client';
+
+export const MEDIA_LIST_QUERY = gql`
+query Page($perPage: Int, $formatIn: [MediaFormat], $genreIn: [String], $sort: [MediaSort]) {
+  Page(perPage: $perPage) {
+    pageInfo {
+      total
+      perPage
+      currentPage
+      lastPage
+      hasNextPage
+    }
+    media(format_in: $formatIn, genre_in: $genreIn, sort: $sort) {
+      id
+      title {
+        english
+      }
+      type
+      format
+      episodes
+      chapters
+      volumes
+      coverImage {
+        large
+      }
+      bannerImage
+      averageScore
+      popularity
+      siteUrl
+    }
+  }
+}g
+`;
