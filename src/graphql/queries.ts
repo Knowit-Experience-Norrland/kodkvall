2bf41d6b154e84f7e278d3bf179cb5a7a591cb59
Alexander <alexander.volkers@knowit.se>
2022-10-26 18:53:52 +0200

    genres

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,33 +1,44 @@
-import {gql} from '@apollo/client';
+import { gql } from "@apollo/client";
+
+export const GENRES_QUERY = gql`
+  query Genres {
+    GenreCollection
+  }
+`;
 
 export const MEDIA_LIST_QUERY = gql`
-query Page($perPage: Int, $formatIn: [MediaFormat], $genreIn: [String], $sort: [MediaSort]) {
-  Page(perPage: $perPage) {
-    pageInfo {
-      total
-      perPage
-      currentPage
-      lastPage
-      hasNextPage
-    }
-    media(format_in: $formatIn, genre_in: $genreIn, sort: $sort) {
-      id
-      title {
-        english
+  query Page(
+    $perPage: Int
+    $formatIn: [MediaFormat]
+    $genreIn: [String]
+    $sort: [MediaSort]
+  ) {
+    Page(perPage: $perPage) {
+      pageInfo {
+        total
+        perPage
+        currentPage
+        lastPage
+        hasNextPage
       }
-      type
-      format
-      episodes
-      chapters
-      volumes
-      coverImage {
-        large
+      media(format_in: $formatIn, genre_in: $genreIn, sort: $sort) {
+        id
+        title {
+          english
+        }
+        type
+        format
+        episodes
+        chapters
+        volumes
+        coverImage {
+          large
+        }
+        bannerImage
+        averageScore
+        popularity
+        siteUrl
       }
-      bannerImage
-      averageScore
-      popularity
-      siteUrl
     }
   }
-}g
 `;
